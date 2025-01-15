d9a354b9f4a2055be83cf0eaa87ef19449aa17dc
unknown <meenakshibhati555@gmail.com>
2025-01-15 22:58:40 +0530

    first commit

---
+++ src/components/ClockHeading.jsx
@@ -0,0 +1,13 @@
+// We can write functon both 2 ways
+
+// First Way :- 
+// function ClockHeading (){
+//     return <h1>Bharat Clock</h1>
+// }
+
+
+// Second Way :- 
+let ClockHeading = () => {
+    return <h1 className="fw-bolder">Bharat Clock</h1>
+};
+export default ClockHeading
